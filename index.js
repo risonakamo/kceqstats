d76c5a7d4686647af601837c348469facbfc67e9
risonakamo <hmykanae@gmail.com>
2018-02-04 14:47:04 -0500

    tested out sorttests

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 window.onload=main;
 
+var _apiAllEquip;
+var _apiEquip;
+
 function main()
 {
+    var r=new XMLHttpRequest();
 
+    r.open("GET","data/api-equip.json");
+
+    r.onreadystatechange=()=>{
+        if (r.readyState==4)
+        {
+            _apiAllEquip=JSON.parse(r.response);
+        }
+    }
+
+    r.send();
+
+    var r2=new XMLHttpRequest();
+
+    r2.open("GET","data/player-equip.json");
+
+    r2.onreadystatechange=()=>{
+        if (r2.readyState==4)
+        {
+            _apiEquip=JSON.parse(r2.response);
+        }
+    }
+
+    r2.send();
 }
