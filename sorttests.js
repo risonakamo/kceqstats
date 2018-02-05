28e18a9844ee345834942bc2e6eaac260073e057
risonakamo <hmykanae@gmail.com>
2018-02-05 15:54:28 -0500

    prepare for eqlist generation sort type/name together function

--- sorttests.js
+++ sorttests.js
@@ -41,3 +41,35 @@ function sortName()
 
     return res;
 }
+
+function sortTypeAndName()
+{
+    var res={};
+    var id,type;
+    for (var x in _apiEquip)
+    {
+        id=_apiEquip[x].api_slotitem_id;
+        type=_apiAllEquip[id].api_type[3];
+
+        if (res[type])
+        {
+            if (res[type][id])
+            {
+                res[type][id].count++;
+            }
+
+            else
+            {
+                res[type][id]={count:1,name:_apiAllEquip[id].api_name};
+            }
+        }
+
+        else
+        {
+            res[type]={};
+            res[type][id]={count:1,name:_apiAllEquip[id].api_name};
+        }
+    }
+
+    return res;
+}
